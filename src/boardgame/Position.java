bcd7b823f8fc91cf5f8b5f82616daa91ffdd87f7
Renato Barbosa <renatocolbar@gmail.com>
2026-02-25 12:03:40 -0300

    ChessException and ChessPosition

--- src/boardgame/Position.java
+++ src/boardgame/Position.java
@@ -3,11 +3,11 @@ package boardgame;
 public class Position {
 	
 	private int row;
-	private int colum;
+	private int column;
 	
 	public Position(int row, int colum) {
 		this.row = row;
-		this.colum = colum;
+		this.column = colum;
 	}
 
 	public int getRow() {
@@ -18,17 +18,17 @@ public class Position {
 		this.row = row;
 	}
 
-	public int getColum() {
-		return colum;
+	public int getColumn() {
+		return column;
 	}
 
 	public void setColum(int colum) {
-		this.colum = colum;
+		this.column = colum;
 	}
 
 	@Override
 	public String toString() {
-		return "Position [row=" + row + ", colum=" + colum + "]";
+		return "Position [row=" + row + ", colum=" + column + "]";
 	}
 	
 
